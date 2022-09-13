d1bea55e402d4c07703b8d682b495ce0cc241d5d
Gayathri Annem <annemgayathri@gmail.com>
2022-09-12 19:59:18 -0500

    Time: 3 ms (18.47%), Space: 42.1 MB (50.74%) - LeetHub

---
+++ 1678-goal-parser-interpretation/1678-goal-parser-interpretation.java
@@ -0,0 +1,26 @@
+class Solution {
+    public String interpret(String command) {
+        String str="";
+        for(int i=0; i<command.length();i++)
+        {
+            if(command.charAt(i)=='G')
+            {
+                str=str+'G';
+            }
+            else if(command.charAt(i)=='(' && command.charAt(i+1)==')')
+            {
+                str=str+'o';
+            }
+            else if(command.charAt(i)=='(' && command.charAt(i+1)=='a')
+            {
+                str=str+"al";
+            }
+            else
+            {
+                str=str;
+            }
+        }
+        return str;
+        
+    }
+}
