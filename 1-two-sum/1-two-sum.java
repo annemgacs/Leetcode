cff381766e7e1ed3528666f42c15f854731369e6
Gayathri Annem <annemgacs@gmail.com>
2022-09-21 11:11:10 -0500

    Time: 100 ms (19.94%), Space: 45 MB (59.60%) - LeetHub

--- 1-two-sum/1-two-sum.java
+++ 1-two-sum/1-two-sum.java
@@ -3,13 +3,13 @@ class Solution {
         int[] ans=new int[2];
         for(int i=0;i<nums.length;i++)
         {
-            for(int j=0;j<nums.length && j!=i; j++)
+            for(int j=i+1;j<nums.length; j++)
             {
                 if(nums[i]+nums[j]==target)
                 {
                     ans[0]=i;
                     ans[1]=j;
-                    break;
+                    
                 }
             }
         }
